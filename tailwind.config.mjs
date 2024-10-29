4aaee5698131fad6189d64c222455ed57447324f
ishii <winryzen01@gmail.com>
2024-10-29 09:45:37 +0900

    最初のコミット

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens:{
+				"sp-md": "400px",
+				"sp-lg": "550px",
+				"break": "720px",
+			},
+			colors:{
+				"cat-pink": "#c03a7d",
+				"moon-blue": "#064072",
+			},
+		},
+		fontFamily: {
+			klee: ["Klee One", "cursive"],
+			notoJP: ["Noto Sans JP","sans-serif"],
+			kaiseiDecol: ["Kaisei Decol", "serif"],
+		},
+		container:{
+			center: true,
+		},
 	},
 	plugins: [],
 }
