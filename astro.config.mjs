8c504f1eb19b85001f99737c1d548bc883887960
seishunkanojo <seishunkanojo01@gmail.com>
2025-03-16 00:13:00 +0900

    画像の最適化とお問い合わせフォームのurl変更 仕様書.mdの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    integrations: [tailwind()],
+    image: {
+        service: {
+            entrypoint: "astro/assets/services/sharp",
+        },
+        // 画像の最適化設定
+        domains: [],
+        remotePatterns: [],
+        format: ["webp", "avif", "png", "jpeg"],
+        quality: 80, // 画質の設定（0-100）
+        densities: [1, 2], // レスポンシブ画像の密度
+    },
 });
